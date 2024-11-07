e04fbc43696d3416888241d2f851ecceb9a72fb5
i513773 <513773@student.fontys.nl>
2024-11-07 11:58:29 +0100

    Exercises toevoegen aan training

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2adb121cf7a1bbfaeb9aa9a4e413886f5159ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc22ceeb107ed86f2b114c0afd9c10b595253fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
