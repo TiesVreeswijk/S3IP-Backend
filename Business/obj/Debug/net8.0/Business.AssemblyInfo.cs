d61ceaf519df0f3eb94e59eee71c5ffa2af51504
Ties Vreeswijk <513773@student.fontys.nl>
2024-11-22 11:06:08 +0100

    Docker aangemaakt

--- Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc8eb9d0df70127afaf92ccee76e8f145384299")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a655a4b0d261f70bd81f9128e75cb73303c2bfca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
