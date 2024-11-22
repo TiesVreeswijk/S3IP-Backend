ef88edd31feabfafcf6975ce15c0cac66bb8d9eb
Ties Vreeswijk <513773@student.fontys.nl>
2024-11-22 14:12:50 +0100

    hoi

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -20,7 +20,7 @@ namespace API
         public IActionResult Login([FromBody] LoginReq request)
         {
             try {
-                var token = userService.LoginUserReturnToken(request); //test
+                var token = userService.LoginUserReturnToken(request); //
                 return Ok(new { token });
             }
             catch (ResourceNotFoundException e) { return BadRequest(e.Message); }
