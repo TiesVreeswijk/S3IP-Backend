7f4b7769bc40ebf7a19cf7cc45b23f9a839de644
i513773 <513773@student.fontys.nl>
2024-12-11 21:02:41 +0100

    cd gefixt

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad94b8e3516f805f1fbe0fb8776d4b667dcd8432")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074ff67aa94db9c77b449400452c8234080daf6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
